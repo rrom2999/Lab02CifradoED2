dbf6a4be528b9c6471075cce38ea42b8f9aba252
Ronald Ovalle <rrom2999@gmail.com>
2019-10-01 17:04:08 -0600

    Vista ZigZag

---
+++ Lab2Cifrados/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2Cifrados.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LlamadoZigZag()
+        {
+            return RedirectToAction("Index", "ZigZag");
+        }
+
+        public ActionResult LlamadoEspiral()
+        {
+            return RedirectToAction("Index", "Espiral");
+        }
+
+        public ActionResult LlamadoCesar()
+        {
+            return RedirectToAction("Index", "Cesar");
+        }
+    }
+}
---
+++ Lab2Cifrados/Controllers/ZigZagController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace Lab2Cifrados.Controllers
+{
+    public class ZigZagController : Controller
+    {
+        // GET: ZigZag
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CargaCifradoZZ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CargaCifradoZZ(HttpPostedFileBase ACifrarZZ, string Clave)
+        {
+            if (ACifrarZZ != null)
+            {
+                var NombreArchivo = ACifrarZZ.FileName;
+                var Niveles = Convert.ToInt32(Clave);
+                var Name = NombreArchivo.Split('.');
+                var RutaLectura = Server.MapPath($"~/Cargados/{NombreArchivo}");
+                var RutaEscritura = Server.MapPath($"~/CifradoZZ/{Name[0]}.cif");
+                ACifrarZZ.SaveAs(RutaLectura);
+
+                const int TBuffer = 1024;
+                var LineaVector = 0;
+                var CLineas = Convert.ToInt32(Clave);
+                var ArregloEscritor = new string[CLineas];
+
+                using (var stream = new FileStream(RutaLectura, FileMode.Open))
+                {
+                    using (var Lector = new BinaryReader(stream))
+                    {
+                        using (var writeStream = new FileStream(RutaEscritura, FileMode.OpenOrCreate))
+                        {
+                            using (var Escritor = new BinaryWriter(writeStream))
+                            {
+
+                            }
+                        }
+                    }
+                }
+            }
+
+            return View();
+        }
+
+    }
+}
