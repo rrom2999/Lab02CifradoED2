175be9b9b20bf2e01baba32a982a82f8198129d8
Ronald Ovalle <rrom2999@gmail.com>
2019-10-17 22:11:18 -0600

    Cifrado SDES sin escritura

--- Lab2Cifrados/Models/SDES.cs
+++ Lab2Cifrados/Models/SDES.cs
@@ -9,6 +9,8 @@ namespace Lab2Cifrados.Models
     {
         public string K1;
         public string K2;
+        public string[,] S0 = new string[4, 4];
+        public string[,] S1 = new string[4, 4];
 
         public string P10(string Binario)  //Cambiar a que lo lea en txt
         {
@@ -64,6 +66,54 @@ namespace Lab2Cifrados.Models
             }
 
             return Nuevo;
+        }
+
+        public string IP(string Binario, bool Inverso)
+        {
+            var Secuencia = "70615243";
+            var Nuevo = "";
+            if (!Inverso)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    var Posicion = Convert.ToInt32(Convert.ToString(Secuencia[i]));
+                    Nuevo = $"{Nuevo}{Binario[Posicion]}";
+                }
+            }
+            else
+            {
+                var SecuenciaInversa = "";
+                string[] VectorInverso = new string[8];
+                for(int i = 0; i < 8; i++)
+                {
+                    VectorInverso[Convert.ToInt32(Convert.ToString(Secuencia[i]))] = Convert.ToString(i);
+                }
+                
+                foreach(var Valor in VectorInverso)
+                {
+                    SecuenciaInversa = $"{SecuenciaInversa}{Valor}";
+                }
+
+                for (int i = 0; i < 8; i++)
+                {
+                    var Posicion = Convert.ToInt32(Convert.ToString(SecuenciaInversa[i]));
+                    Nuevo = $"{Nuevo}{Binario[Posicion]}";
+                }
+            }
+
+            return Nuevo;
+        }
+
+        public string XOR(string Uno, string Dos)
+        {
+            var ResultadoXOR = "";
+            for (int i = 0; i < Uno.Length; i++)
+            {
+                if (Uno[i] != Dos[i]) ResultadoXOR += "1";
+                else ResultadoXOR += "0";
+            }
+
+            return ResultadoXOR;
         }
 
         public void ObtenerKas(string Llave)
@@ -124,6 +174,65 @@ namespace Lab2Cifrados.Models
             }
             ShifteadoDos = $"{ShifteadoDos}{Temporal}{ParteB[0]}{ParteB[1]}";
             K2 = P8(ShifteadoDos);
+        }
+
+        public string ObtenerDeSBoxes(string Binario)
+        {
+            var AuxFila = $"{Binario[3]}{Binario[0]}"; var AuxCol = $"{Binario[2]}{Binario[1]}"; //Al reves para facilitar la conversion a int
+            var FilaS0 = 0; var ColS0 = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                if (AuxFila[i] == '1')
+                    FilaS0 += (int)Math.Pow(2, i);
+
+                if (AuxCol[i] == '1')
+                    ColS0 += (int)Math.Pow(2, i);
+            }
+
+            AuxFila = $"{Binario[7]}{Binario[4]}"; AuxCol = $"{Binario[6]}{Binario[5]}";  //Al reves para facilitar conversion a int
+            var FilaS1 = 0; var ColS1 = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                if (AuxFila[i] == '1')
+                    FilaS1 += (int)Math.Pow(2, i);
+
+                if (AuxCol[i] == '1')
+                    ColS1 += (int)Math.Pow(2, i);
+            }
+
+            var ResultadoSBoxes = $"{S0[FilaS0, ColS0]}{S1[FilaS1, ColS1]}";
+            return ResultadoSBoxes;
+        }
+
+        public string CifrarByte(string ByteLeido)
+        {
+            var Binario = IP(ByteLeido, false); //Paso1
+
+            var ParteA = Binario.Substring(0, 4);
+            var ParteB = Binario.Substring(4, 4);
+
+            var NuevaParteB = EP(ParteB); //Paso2
+            NuevaParteB = XOR(NuevaParteB, K1); //Paso3
+            
+            var ResultadoSBoxes1 = ObtenerDeSBoxes(NuevaParteB); //Paso4
+            ResultadoSBoxes1 = P4(ResultadoSBoxes1); //Paso5
+            var ParteC = XOR(ResultadoSBoxes1, ParteA); //Paso6
+
+            var NuevoBinario = $"{ParteB}{ParteC}";//Paso7 y Paso8
+
+            var ParteD = NuevoBinario.Substring(0, 4);
+            var ParteE = NuevoBinario.Substring(4, 4);
+
+            var NuevaParteE = EP(ParteE); //Paso9
+            NuevaParteE = XOR(NuevaParteE, K2); //Paso10
+
+            var ResultadoSBoxes2 = ObtenerDeSBoxes(NuevaParteE); //Paso11
+            ResultadoSBoxes2 = P4(ResultadoSBoxes2); //Paso12
+            var ParteF = XOR(ResultadoSBoxes2, ParteD); //Paso13
+
+            var Nuevo = $"{ParteF}{ParteE}"; //Paso14
+            Nuevo = IP(Nuevo, true); //Paso15
+            return Nuevo;
         }
     }
 }
